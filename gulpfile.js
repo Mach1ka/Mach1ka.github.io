bfb74f6f4d25117986cdb07556f9049453de873d
Machika <59318169+Mach1ka@users.noreply.github.com>
2022-11-06 00:02:47 +0300

    comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,48 @@
 const gulp = require("gulp");
-const browserSync = require("browser-sync");
-const sass = require("gulp-sass")(require("sass"));
 const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
 const autoprefixer = require("gulp-autoprefixer");
+const sass = require("gulp-sass")(require("sass"));
 const rename = require("gulp-rename");
 
-gulp.task("server", function () {
-    browserSync({
-        server: {
-            baseDir: "src",
-        },
-    });
+const browserSync = require("browser-sync").create();
 
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+gulp.task("copy", function () {
+    return gulp.src("src/**/*.*").pipe(gulp.dest("dest/"));
 });
 
-gulp.task("styles", function () {
+gulp.task("sassToCSS", function () {
     return gulp
-        .src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-        .pipe(rename({ suffix: ".min", prefix: "" }))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({ compatibility: "ie8" }))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+        .src("src/scss/**/*.scss")
+        .pipe(
+            sass({
+                errorLogToConsole: true,
+                outputStyle: "compressed",
+            })
+        )
+        .on("error", console.error.bind(console))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("dest/css/"))
+        .pipe(gulp.dest("src/css"));
+});
+
+gulp.task("minifyJS", function () {
+    return gulp
+        .src("src/scripts/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("dest/js/"));
+});
+
+gulp.task("server", function () {
+    browserSync.init({
+        server: "src",
+    });
+    browserSync.watch("src/**/*.*").on("change", browserSync.reload);
 });
 
 gulp.task("watch", function () {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/scss/**/*.scss", gulp.series("sassToCSS"));
+    gulp.watch("src/scripts/*.js", gulp.series("minifyJS"));
 });
 
-gulp.task("default", gulp.parallel("watch", "server", "styles"));
+gulp.task("default", gulp.parallel("watch", "server"));
